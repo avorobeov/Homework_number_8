25ddc586f911d9d413403091a89adafab00e311b
воробйов александр <avorobeov21@gmail.com>
2023-04-14 18:56:09 +0300

    sa

---
+++ Homework_number_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSms;
+
+            Console.Write("Укажите сколько раз вам вывести смс: ");
+            numberOfSms = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfSms; i++)
+            {
+                Console.WriteLine($"Смс номер: {i}");
+            }
+        }
+    }
+}
